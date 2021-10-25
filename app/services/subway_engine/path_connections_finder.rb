6b4c70a6b818abcab2caa7d2b0ba2300ef7ced2e
Temoc Palma <temopalmag@gmail.com>
2021-10-25 10:24:44 -0500

    Refactor service path connections finder considering nearest station to source

--- app/services/subway_engine/path_connections_finder.rb
+++ app/services/subway_engine/path_connections_finder.rb
@@ -1,37 +1,52 @@
 module SubwayEngine
   class PathConnectionsFinder < ApplicationService
-    def initialize(subway_data, source_lines, destination_lines)
+    def initialize(subway_data, source, source_lines, destination_lines)
       @subway_data = subway_data
+      @source = source
       @source_lines = source_lines
       @destination_lines = destination_lines
     end
 
     def call
-      get_path_connections(@source_lines, @destination_lines, 0, [])
+      get_path_connections(0, [])
     end
 
     private
 
-    def get_path_connections(source_lines, destination_lines, index, path_connections)
-      return {source_line: source_lines[index - 1], connections: path_connections} unless path_connections.empty?
-      return {} unless index < source_lines.size
+    def get_path_connections(index, path_connections)
+      return {source_line: @source_lines[index - 1], connections: path_connections} unless path_connections.empty?
+      return {} unless index < @source_lines.size
 
-      line = @subway_data.find { |line| line[:name] == source_lines[index] }
+      line = @subway_data.find { |line| line[:name] == @source_lines[index] }
       connections_of_line = ConnectionsOfLineFinder.call(line, @subway_data)
       connection_names = connections_line_names(connections_of_line)
       intersection_lines = IntersectionBetweenLinesFinder.call(
         connection_names,
-        destination_lines
+        @destination_lines
       )
 
       path_connections = intersection_lines.map do |inter|
         connections_of_line.find { |conn| conn[:line] == inter }
       end
-      get_path_connections(source_lines, destination_lines, index+=1, path_connections)
+      path_connections = select_nearest_connection(path_connections, line)
+      get_path_connections(index+=1, path_connections)
     end
 
     def connections_line_names(connections_line)
       connections_line.map { |conn| conn[:line] }
+    end
+
+    def select_nearest_connection(path_connections, line)
+      return path_connections if path_connections.size < 2
+
+      path_connections_with_count_stations = path_connections.map do |conn|
+        conn[:count_stations] = Stations::CountBetweenStationsCalculator.call(line, @source, conn[:station])
+        conn
+      end
+
+      sort_path_connections = path_connections_with_count_stations.sort { |l1, l2| l1[:count_stations] <=> l2[:count_stations] }
+
+      [sort_path_connections.first]
     end
 
   end
--- spec/services/subway_engine/path_connections_finder_spec.rb
+++ spec/services/subway_engine/path_connections_finder_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe SubwayEngine::PathConnectionsFinder, type: :model do
   let(:subway_data) { SubwayDescriptor.call }
   describe 'call when source and destination lines have one line only' do
     context 'and they intersect' do
+      let(:source) { "PORTALES" }
       let(:source_lines) { ["LINEA 2"] }
       let(:destination_lines) { ["LINEA 12"] }
-      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source_lines, destination_lines)}
+      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source, source_lines, destination_lines)}
 
       it 'returns the path list is not empty' do
         expect(result).not_to be_empty
@@ -23,9 +24,10 @@ RSpec.describe SubwayEngine::PathConnectionsFinder, type: :model do
     end
 
     context 'and they do not intersect' do
+      let(:source) { "PORTALES" }
       let(:source_lines) { ["LINEA 2"] }
       let(:destination_lines) { ["LINEA 5"] }
-      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source_lines, destination_lines)}
+      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source, source_lines, destination_lines)}
 
       it 'returns the path list is not empty' do
         expect(result).to be_empty
@@ -36,9 +38,10 @@ RSpec.describe SubwayEngine::PathConnectionsFinder, type: :model do
   describe 'call when there are several source lines and destination lines has one line only' do
 
     context 'and they intersect through first source line only' do
+      let(:source) { "ERMITA" }
       let(:source_lines) { ["LINEA 2", "LINEA 12"] }
       let(:destination_lines) { ["LINEA 9"] }
-      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source_lines, destination_lines)}
+      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source, source_lines, destination_lines)}
 
       it 'returns the path list is not empty' do
         expect(result).not_to be_empty
@@ -55,9 +58,10 @@ RSpec.describe SubwayEngine::PathConnectionsFinder, type: :model do
     end
 
     context 'and they intersect through second source line only' do
+      let(:source) { "TACUBA" }
       let(:source_lines) { ["LINEA 2", "LINEA 7"] }
       let(:destination_lines) { ["LINEA 6"] }
-      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source_lines, destination_lines)}
+      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source, source_lines, destination_lines)}
 
       it 'returns the path list is not empty' do
         expect(result).not_to be_empty
@@ -74,9 +78,10 @@ RSpec.describe SubwayEngine::PathConnectionsFinder, type: :model do
     end
 
     context 'and they do not intersect' do
+      let(:source) { "TACUBA" }
       let(:source_lines) { ["LINEA 2", "LINEA 7"] }
       let(:destination_lines) { ["LINEA 5"] }
-      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source_lines, destination_lines)}
+      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source, source_lines, destination_lines)}
 
       it 'returns the path list is not empty' do
         expect(result).to be_empty
@@ -87,9 +92,10 @@ RSpec.describe SubwayEngine::PathConnectionsFinder, type: :model do
   describe 'call when there are several source lines and destination lines' do
 
     context 'and they intersect through first source line only with one destination line only' do
+      let(:source) { "ERMITA" }
       let(:source_lines) { ["LINEA 2", "LINEA 12"] }
       let(:destination_lines) { ["LINEA 4", "LINEA 9"] }
-      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source_lines, destination_lines)}
+      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source, source_lines, destination_lines)}
 
       it 'returns the path list is not empty' do
         expect(result).not_to be_empty
@@ -106,9 +112,10 @@ RSpec.describe SubwayEngine::PathConnectionsFinder, type: :model do
     end
 
     context 'and they intersect through first source line only with all destination lines' do
+      let(:source) { "ERMITA" }
       let(:source_lines) { ["LINEA 2", "LINEA 12"] }
       let(:destination_lines) { ["LINEA 3", "LINEA 9"] }
-      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source_lines, destination_lines)}
+      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source, source_lines, destination_lines)}
 
       it 'returns the path list is not empty' do
         expect(result).not_to be_empty
@@ -126,9 +133,10 @@ RSpec.describe SubwayEngine::PathConnectionsFinder, type: :model do
     end
 
     context 'and they intersect through second line only with one destination line only' do
+      let(:source) { "HIDALGO" }
       let(:source_lines) { ["LINEA 2", "LINEA 3"] }
       let(:destination_lines) { ["LINEA 4", "LINEA 6"] }
-      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source_lines, destination_lines)}
+      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source, source_lines, destination_lines)}
 
       it 'returns the path list is not empty' do
         expect(result).not_to be_empty
@@ -145,9 +153,10 @@ RSpec.describe SubwayEngine::PathConnectionsFinder, type: :model do
     end
 
     context 'and they intersect through second line only with all destination lines' do
+      let(:source) { "HIDALGO" }
       let(:source_lines) { ["LINEA 2", "LINEA 3"] }
       let(:destination_lines) { ["LINEA 5", "LINEA 6"] }
-      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source_lines, destination_lines)}
+      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source, source_lines, destination_lines)}
 
       it 'returns the path list is not empty' do
         expect(result).not_to be_empty
@@ -165,9 +174,10 @@ RSpec.describe SubwayEngine::PathConnectionsFinder, type: :model do
     end
 
     context 'and they do not intersect' do
+      let(:source) { "MARTIN CARRERA" }
       let(:source_lines) { ["LINEA 4", "LINEA 6"] }
       let(:destination_lines) { ["LINEA 2", "LINEA 12"] }
-      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source_lines, destination_lines)}
+      let(:result) { SubwayEngine::PathConnectionsFinder.call(subway_data, source, source_lines, destination_lines)}
 
       it 'returns the path list is not empty' do
         expect(result).to be_empty
