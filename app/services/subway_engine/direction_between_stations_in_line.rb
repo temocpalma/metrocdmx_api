94c27c65efb28698947fd552df6529ec075e6ac2
Temoc Palma <temopalmag@gmail.com>
2021-10-25 10:14:36 -0500

    Refactor service direction between stations using new services

--- app/services/subway_engine/direction_between_stations_in_line.rb
+++ app/services/subway_engine/direction_between_stations_in_line.rb
@@ -13,23 +13,10 @@ module SubwayEngine
 
     private
 
-    def get_target_line
-      @subway_data.find do |line|
-        line[:name] == @line
-      end
-    end
-
-    def get_station_pos(target_line, target_station)
-      stations_names = target_line[:stations].map do |station|
-        station[:name]
-      end
-      stations_names.find_index(target_station)
-    end
-
     def direction
-      line = get_target_line
-      source_pos = get_station_pos(line, @source)
-      destination_pos = get_station_pos(line, @destination)
+      line = Lines::LineByNameFinder.call(@subway_data, @line)
+      source_pos = Stations::StationPositionInLineFinder.call(line, @source)
+      destination_pos = Stations::StationPositionInLineFinder.call(line, @destination)
 
       return "#{@source} does not exists in #{@line}" if source_pos.nil?
       return "#{@destination} does not exists in #{@line}" if destination_pos.nil?
