e81772af8e12872a85748fce959a6a74fbd86d20
Temoc Palma <temopalmag@gmail.com>
2021-10-29 11:54:04 -0500

    Move connections of line finder service into Lines module

--- app/services/subway_engine/lines/connections_of_line_finder.rb
+++ app/services/subway_engine/lines/connections_of_line_finder.rb
@@ -1,4 +1,4 @@
-module SubwayEngine
+module SubwayEngine::Lines
   class ConnectionsOfLineFinder < ApplicationService
     def initialize(target_line, all_lines)
       @target_line = target_line
@@ -10,7 +10,7 @@ module SubwayEngine
       connections_lines = []
       @target_line[:stations].each do |station|
         searching_lines.each do |line|
-          connections_lines << {line: line[:name], station: station[:name]} if Stations::StationIsInLineValidator.call(station[:name], line)
+          connections_lines << {line: line[:name], station: station[:name]} if SubwayEngine::Stations::StationIsInLineValidator.call(station[:name], line)
         end
       end
       connections_lines.sort { |l1, l2| l1[:line] <=> l2[:line] }
