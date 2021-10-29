ad2cdfbe0db2349f798085ff536b25ca5a759b00
Temoc Palma <temopalmag@gmail.com>
2021-10-29 12:59:00 -0500

    Adjust using new segment creator service

--- app/services/subway_engine/result_path_creator.rb
+++ app/services/subway_engine/result_path_creator.rb
@@ -13,29 +13,18 @@ module SubwayEngine
       line = @path_connections[:source_line]
       destination = @path_connections[:connections].first[:station]
       count_stations = @path_connections[:connections].first[:count_stations]
-      start_segment = create_segment(line, @source, destination)
+      start_segment = SegmentCreator.call(@subway_data, line, @source, destination)
       result_path << start_segment
 
       tail_segments = @path_connections[:connections].each_with_index.map do |conn, index|
         destination = get_destination(@path_connections[:connections][index + 1])
-        segment = create_segment(conn[:line], conn[:station], destination)
+        segment = SegmentCreator.call(@subway_data, conn[:line], conn[:station], destination)
         result_path << segment
       end
       result_path
     end
 
     private
-
-    def create_segment(line, source, destination)
-      line_data = Lines::LineByNameFinder.call(@subway_data, line)
-      {
-        line: line,
-        source: source,
-        destination: destination,
-        direction: DirectionBetweenStationsInLine.call(@subway_data, line, source, destination),
-        count_stations: Stations::CountBetweenStationsCalculator.call(line_data, source, destination)
-      }
-    end
 
     def get_destination(destination)
       return @destination if destination.nil?
